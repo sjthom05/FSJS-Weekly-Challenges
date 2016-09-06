1f736d6e091649ba3041279aaad3d9609d0c7828
Scott Thompson <sjthom05@gmail.com>
2016-09-06 18:45:54 -0400

    Solution to the week 2 code challenge

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,8 +1,25 @@
+var userInput = ""
+
 //create function
+function helloWorld (input)
+{
+  var output = ""
   //control and return statements
+  if(input.toLowerCase() === 'hello')
+  {
+  	output = 'Hello World'
+  }
+  else
+  {
+  	output = 'That is not hello!'
+  }
+
+  return output
+}
 
 //Prompt user for input and store variable
+userInput = prompt("Type Hello!")
 
 //Call function
-
 //Alert user results
+alert(helloWorld(userInput))
